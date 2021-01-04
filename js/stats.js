21f6dc3d96a9a5da8a0037e3aa5b0134f3af1cc0
LevanGelashvili <lgela17@freeuni.edu.ge>
2021-01-05 01:19:19 +0400

    Add event listeners, fetch stats

---
+++ js/stats.js
@@ -0,0 +1,11 @@
+// document.querySelectorAll('img').forEach(item => {
+//     item.addEventListener("click", function() {
+//         alert("Hi............")
+//     })
+// })
+
+// const getStats = () => {
+//     fetch('https://www.balldontlie.io/api/v1/players?search=kemba_walker').then(response => {
+//         console.log(response)
+//     })
+// }

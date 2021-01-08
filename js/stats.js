5e2bca76d241a2654a3031b45e0a42c1a350d3d8
LevanGelashvili <lgela17@freeuni.edu.ge>
2021-01-08 23:24:05 +0400

    Add null returned stat parameters

--- js/stats.js
+++ js/stats.js
@@ -11,13 +11,13 @@ document.querySelectorAll('figure').forEach(item => {
         }).then(playerDataJson => {
             
             let data = playerDataJson.data[0]
-            let name = data.first_name + ' ' + data.last_name
 
             fetch(`https://www.balldontlie.io/api/v1/season_averages?season=2010&player_ids[]=${data.id}`).then(response => {
                 return response.json()
             }).then(seasonAvgsJson => {
 
-                loadStatsAvatar(name)
+                loadStatsParams(data)
+                loadStatsAvatar(data.first_name + ' ' + data.last_name)
 
                 let statsPanel = document.getElementsByClassName('stats')[0]
                 statsPanel.style.display = 'flex'
@@ -28,9 +28,45 @@ document.querySelectorAll('figure').forEach(item => {
     })
 })
 
-function loadStatsAvatar(name) {
+function loadStatsParams(data) {
+
+    var name = data.first_name + ' ' + data.last_name
+    var height = 'Height: '
+    var weight = 'Weight: '
+
+    switch (name) {
+        case 'Omer Asik':
+            height += "7'0"
+            weight += "255"
+            break
+        case 'Carlos Boozer':
+            height += "6'9"
+            weight += "258"
+            break
+        case 'Ronnie Brewer':
+            height += "6'7"
+            weight += "235"
+            break
+        case 'C.J. Watson':
+            height += "6'2"
+            weight += "175"
+            break
+        case 'Keith Bogans':
+            height += "6'5"
+            weight += "215"
+            break
+        default:
+            height += data.height_feet + "'" + data.height_inches
+            weight += data.weight_pounds
+    }
 
     document.getElementById('stats-name').innerHTML = name
+    document.getElementById('stats-height').innerHTML = height
+    document.getElementById('stats-weight').innerHTML = weight
+}
+
+function loadStatsAvatar(name) {
+
     let image = document.getElementById('stats-avatar')
     
     if (name === 'Derrick Rose') {
